d841c7a0f2805d5010eab2d2ed9dd688d4dbacf8
Brandon Sears <brandonsears8@gmail.com>
2018-04-04 14:30:45 -0400

    add triangles and fix ellipse on click

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 // setup variables for app
 let color = {r: 0, g: 0, b: 255}
+let size = 15
+let mode = 'draw'
 
 // set document variables
 const recentColorList = document.querySelector('.recent-colors')
 const savedColorList = document.querySelector('.saved-colors')
+const sizeSlider = document.querySelector('.sizeSlider')
+const squareToggle = document.querySelector('.square')
+
+//size slider event listener
+sizeSlider.addEventListener('input', (e) => {
+  document.querySelector('.size').innerHTML = `Size: ${e.target.value}`
+  size = e.target.value
+})
+
+squareToggle.addEventListener('click', () => {
+  mode = 'square'
+})
+
+document.querySelector('.clear').addEventListener('click', () => {
+  clear()
+})
+document.querySelector('.draw').addEventListener('click', () => {
+  mode = 'draw'
+})
+document.querySelector('.triangle').addEventListener('click', () => {
+  mode = 'triangle'
+})
+
+
 // setup color picker from iro.js
 const colorPicker = new iro.ColorPicker(".color-picker", {
   width: 130,
@@ -57,7 +83,10 @@ function setup() {
     {r: 0, g: 0, b: 255},
     hexString: '#00F'
   })
+  frameRate(200)
 }
+
+
 
 function draw() {
 }
@@ -71,13 +100,41 @@ function keyPressed() {
 
 function mouseDragged(){
 // the mouse detection starts at top of canvas so above this is negative Y thats why I check for > 0 to make sure this is only happening on canvas mouse drag
-    if(mouseY > 0){
+    if(mouseY > 0 && mode === 'draw'){
       smooth()
-      strokeWeight(20)
+      strokeWeight(size)
       stroke(color.r, color.g, color.b)
       line(mouseX, mouseY, pmouseX, pmouseY)
+      console.log(size)
+    } else if(mouseY > 0 && mode === 'square'){
+      fill(color.r, color.g, color.b)
+      noStroke()
+      rect(mouseX, mouseY, size, size)
+    } else if(mouseY > 0 && mode === 'triangle'){
+      noStroke()
+      fill(color.r, color.g, color.b)
+      triangle(mouseX-size/1.5,mouseY+size/2,mouseX+size/1.5,mouseY+size/2,mouseX,mouseY)
     }
 }
+
+function mouseClicked() {
+  if(mouseY > 0 && mode === 'square'){
+    fill(color.r, color.g, color.b)
+    noStroke()
+    rect(mouseX, mouseY, size, size)
+  } else if (mouseY > 0 && mode === 'draw'){
+    noStroke()
+    fill(color.r, color.g, color.b)
+    smooth()
+    ellipse(mouseX, mouseY, size/1, size/1)
+    // rect(mouseX, mouseY, size, size)
+  } else if (mouseY > 0 && mode === 'triangle'){
+    noStroke()
+    fill(color.r, color.g, color.b)
+    triangle(mouseX-size/1.5,mouseY+size/2,mouseX+size/1.5,mouseY+size/2,mouseX,mouseY)
+  }
+}
+
 
 // make size slider
 // make buttons to change if making lines or shapes
